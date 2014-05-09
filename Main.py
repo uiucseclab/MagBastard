90b7fe4295acf7d7c7c4d3a836f15fa70e68314e
Magnanimous Bastard User <magbastard@spring2014azure.(none)>
2014-05-09 02:55:13 +0000

    added threading for Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,46 @@
-from resources import ServiceListener
-#from HTTP import HTTPService
-#from SSH import SSHService
-from NetBIOS import NetBIOSService
-#from SMTP import SMTPService
-#from FTP import FTPService
+from threading import Thread
 
-#ServiceListener.startListener(HTTPService.httpHandler, 80, "HTTP/servers.config")
-#ServiceListener.startListener(FTPService.ftpHandler, 21, "FTP/servers.config")
-#ServiceListener.startListener(SMTPService.smtpHandler, 25, "SMTP/servers.config")
-ServiceListener.startListener(NetBIOSService.netbiosHandler, 139, "NetBIOS/servers.config")
+from resources import ServiceListener
+from HTTP import HTTPService
+from SSH import SSHService
+from NetBIOS import NetBIOSService
+from SMTP import SMTPService
+from FTP import FTPService
+
+def SSHThreadFunc():
+    ServiceListener.startListener(SSHService.sshHandler, 10022, "SSH/servers.config")
+    
+def HTTPThreadFunc():
+    ServiceListener.startListener(HTTPService.httpHandler, 10080, "HTTP/servers.config")
+
+def FTPThreadFunc():
+    ServiceListener.startListener(FTPService.ftpHandler, 10021, "FTP/servers.config")
+
+def SMTPThreadFunc():
+    ServiceListener.startListener(SMTPService.smtpHandler, 10025, "SMTP/servers.config")
+
+def NetBIOSThreadFunc():
+    ServiceListener.startListener(NetBIOSService.netbiosHandler, 10139, "NetBIOS/servers.config")
+
+
+SSHthread = Thread(target = SSHThreadFunc)
+SSHthread.start()
+
+HTTPthread = Thread(target = HTTPThreadFunc)
+HTTPthread.start()
+
+FTPthread = Thread(target = FTPThreadFunc) 
+FTPthread.start()
+
+SMTPthread = Thread(target = SMTPThreadFunc)
+SMTPthread.start()
+
+NetBIOSthread = Thread(target = NetBIOSThreadFunc)
+NetBIOSthread.start()
+
+
+SSHthread.join()
+HTTPthread.join()
+FTPthread.join()
+SMTPthread.join()
+NetBIOSthread.join()
